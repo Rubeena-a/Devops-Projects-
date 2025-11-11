0e413ecfeca91fbe51b3ae18d3f2ae9ed8ee5f90
Rubeena-a <rubeenashaik889@gmail.com>
2025-11-11 16:05:08 +0530

    Update app.js

--- CICD-Pipeline-with-GitHub-Actions-Docker/src/app.js
+++ CICD-Pipeline-with-GitHub-Actions-Docker/src/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+
+const createApp = () => {
+  const app = express();
+  app.get('/', (req, res) => {
+    res.json({ message: "Hello from CI/CD demo app!" });
+  });
+  return app;
+};
+
+module.exports = createApp;
 
